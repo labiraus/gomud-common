5ffd79926e9195fd5691933212f1eb37623f013c
Oliver Hathaway <oliver.hathaway@bjss.com>
2021-04-16 07:52:57 +0100

    Added retry loop to db

--- db/db.go
+++ db/db.go
@@ -3,6 +3,7 @@ package db
 import (
 	"context"
 	"log"
+	"time"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -12,15 +13,21 @@ var db *gorm.DB
 
 //Setup connects to database instance
 func Setup(ctx context.Context) {
-	go func() {
-		<-ctx.Done()
-	}()
+	for {
+		var err error
+		dsn := "host=gomud-db-postgres.gomud.svc.cluster.local user=gomud password=admin123 dbname=gomud port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+		db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
-	var err error
-	dsn := "host=gomud-db-postgresql.gomud.svc.cluster.local user=gomud password=admin123 dbname=gomud port=5432 sslmode=disable TimeZone=Asia/Shanghai"
-	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+		if err != nil {
+			log.Println("Error creating connection pool: " + err.Error())
+			log.Println("Waiting for 5 seconds before retrying")
+		} else {
+			return
+		}
 
-	if err != nil {
-		log.Fatal("Error creating connection pool: " + err.Error())
+		select {
+		case <-ctx.Done():
+		case <-time.After(5 * time.Second):
+		}
 	}
 }
